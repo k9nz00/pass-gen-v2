c3a2e837a0e20d877ea9bcd2fde7393927d1534e
SemkaAY <k9nz00@yandex.ru>
2025-11-27 12:50:23 +0300

    init project

---
+++ src/main/java/com/semka/pets/pass_gen_v2/PassGenV2Application.java
@@ -0,0 +1,13 @@
+package com.semka.pets.pass_gen_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassGenV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassGenV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/semka/pets/pass_gen_v2/PassGenV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semka.pets.pass_gen_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassGenV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
